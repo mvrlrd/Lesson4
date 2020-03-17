2012b87b03f515da03932c34f52c093fc9c6bcac
mvrlrd <igrbelykh@gmail.com>
2020-03-17 14:12:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/mvrlrd/lesson4/MainActivity.java
@@ -0,0 +1,24 @@
+package ru.mvrlrd.lesson4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+//Создать GsonActivity.
+// В активити распарсить json-строчку в класс {"time_of_year": "summer", "year": 2019}
+// с помощью Gson.
+// Вывести в лог поля получившегося класса.
+
+//Создать RetrofitActivity.
+// В активити получить строчку с https://api.github.com/users/JakeWharton
+// и отобразить аватарку по avatar_url.
+// Использовать библиотеку retrofit.
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
